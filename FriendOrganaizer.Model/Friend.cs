c95c4b5419657f831ea714d5032d2e46525e2fb0
Andrew Evsugin <ondrya@yandex.ru>
2021-01-10 22:24:16 +0300

    Validate by Using Data Annotations

--- FriendOrganaizer.Model/Friend.cs
+++ FriendOrganaizer.Model/Friend.cs
@@ -5,12 +5,16 @@ namespace FriendOrganaizer.Model
     public class Friend
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(50)]
         public string FirstName { get; set; }
+        
         [StringLength(50)]
         public string LastName { get; set; }
+        
         [StringLength(100)]
+        [EmailAddress]
         public string Email { get; set; }
 
         //public string FullName { 
